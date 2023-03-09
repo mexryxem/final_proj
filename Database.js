f2f8ce7fa009956771bd20f777f787e7fcd27c47
MaxWhite2024 <mjwhite@scu.edu>
2023-03-08 18:40:26 -0800

    update 2
    N/A

--- Database.js
+++ Database.js
@@ -1,6 +1,39 @@
 //Database for COEN 163
-//The database is an array of items
-const items = {
-    Name: "Nike Red Shoe",
-    Location: (2, 5),
-};
+//
+//The database is an array of items.
+//The database variable is called "Items"
+//
+//Each item is an object that stores the:
+//      name (a string), 
+//      location (a tuple with x and y), 
+//      description (a string),
+//      image (a string containing a web URL)
+//
+//Here is a template of an item for easy copy and pasting
+/*
+name: "",
+location: (,),
+description: "".
+image: ""
+*/
+
+let Items = [
+    {
+        name: "Nike Red Shoe",
+        location: (2, 5),
+        description: "It is a red shoe",
+        image: "https://n4.sdlcdn.com/imgs/a/6/l/Nike-Red-Basketball-Sports-Shoes-SDL413059472-1-32cc8.jpg"
+    },
+    {
+        name: "Nike Blue Shoe",
+        location: (2, 6),
+        description: "It is a blue shoe",
+        image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fn1.sdlcdn.com%2Fimgs%2Fb%2Fm%2F4%2FNike-Blue-Running-Shoes-SDL685685373-1-fe96c.jpg&f=1&nofb=1&ipt=19df762439f46020e6d2a1c669d410738af69d46606b7cc7bd220d1c211c2049&ipo=images"
+    },
+    {
+        name: "Nike Yellow Shoe",
+        location: (2,7),
+        description: "It is a yellow shoe".
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdna.lystit.com%2Fphotos%2Fstockx%2F6e0bb617%2Fnike-Varsity-MaizeVarsity-Maize-Ligh-Air-Max-90-Vt-Varsity-Maize.jpeg&f=1&nofb=1&ipt=85053b2830720f886f5ee74431f87993cdba2f12ba28cb32ae58052daa39f568&ipo=images"
+    }
+]
