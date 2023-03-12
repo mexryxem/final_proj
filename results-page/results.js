ecec4df95a29cb9d1740005c75f92eeb052e3fe7
Meryem Esa <meryem.esa@gmail.com>
2023-03-11 21:31:24 -0800

    update results js to work with results html

--- results-page/results.js
+++ results-page/results.js
@@ -1,6 +1,6 @@
 
 //wait for databse to load before trying to access it
-setTimeout(getResultsPage, 1000);
+setTimeout(getResultsPage, 100);
 
 function getResultsPage(){
     //get query from url search parameters
@@ -9,50 +9,20 @@ function getResultsPage(){
 
     //get list of items from database
     let items = Search_Database(query);
-
-    //display items to page
-    let displayItemsDiv = document.querySelector("#display-results");
     
-    //for each item in results
-    for(let i = 0; i < items.length; i++){
-        //create a section for the item and add to the doc
-        let itemRow = document.createElement("div");
-        itemRow.classList = "itemRow"; //<----------style html using item class
-        displayItemsDiv.appendChild(itemRow);
+    //grab all buttons in 'results' div
+    let itemButtons = document.querySelectorAll("div.results > button");
 
-        //create the text element and add it to the row
-        let itemName= items[i]["name"];
-        let itemDescription = items[i]["description"];
-        
-        let p = document.createElement("p");
-        p.innerText = itemName + ": " + itemDescription;
+    //loop through each result button and add info for each result
+    for(let i = 0; i < itemButtons.length; i++){
+        let itemButton = itemButtons[i];
+        let nameDiv = itemButton.querySelector("#items");
+        nameDiv.innerHTML = items[i]["name"];
 
-        itemRow.appendChild(p);
-
-        //create img element and add to row
-        let itemImg = document.createElement("img");
-        itemImg.src = items[i]["image"];
-        itemRow.appendChild(itemImg);
-
-        //create arrow button and add to row
-        let button = document.createElement("button");
-        button.id = items[i]["name"];
-        button.classList = "itemButton";
-        itemRow.appendChild(button);
-    }
-
-    //when button clicked go to corresponding item's map page
-
-    //get list of all buttons
-    let buttons = document.querySelectorAll("button.itemButton");
-
-    //console.log(buttons);
-    for(let i=0; i< buttons.length; i++){
-        //add event listner for each button
-        buttons[i].addEventListener("click", (event)=>{
-            //go to maps page and save the selected item's name through url search parameter
-            location.href = "../maps-page/map.html?itemName=" + buttons[i].id;
-        })
+        //go to correct maps page when clicked
+        itemButton.addEventListener("click", ()=>{
+            location.href = "../maps-page/map.html?itemName=" + items[i]["name"];
+        });
     }
 }
 
