5fec7503324bc877822aea5faeaf73731969212d
Meryem Esa <meryem.esa@gmail.com>
2023-03-12 19:04:32 -0700

    display item info in maps page

--- maps-page/map.js
+++ maps-page/map.js
@@ -12,4 +12,22 @@ function getItem(){
 
   theItem = searchItem(search);
   console.log(theItem);
+
+  
+  let itemDiv = document.querySelector(".item");
+  
+  let nameDiv = document.createElement("p");
+  nameDiv.innerHTML= theItem["name"];
+  itemDiv.appendChild(nameDiv);
+
+  let img = document.createElement("img");
+  img.src = theItem["image"];
+  img.classList="itemImg";
+  itemDiv.appendChild(img);
+
+  let locationDiv = document.createElement("p");
+  locationDiv.innerHTML = theItem["location"];
+  itemDiv.appendChild(locationDiv);
+
+
 }
